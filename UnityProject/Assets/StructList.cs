1c5f18deb16dd04bcf1b416b44b8d62d5dd97565
KID <50666207+KID421@users.noreply.github.com>
2019-12-18 14:50:43 +0800

    找到結構實值型別變成參考型別的問題 (陣列)

---
+++ UnityProject/Assets/StructList.cs
@@ -0,0 +1,14 @@
+﻿[System.Serializable]
+public struct StructData
+{
+    public string character;
+    public int lv;
+    public float atk;
+
+    public StructData(string character, int lv, float atk)
+    {
+        this.character = character;
+        this.lv = lv;
+        this.atk = atk;
+    }
+}
---
+++ UnityProject/Assets/TestStruct.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+public class TestStruct : MonoBehaviour
+{
+    public StructData character1 = new StructData("KID", 50, 70.5f);
+    public StructData character2 = new StructData("TOM", 30, 20.2f);
+
+    public StructData[] characters =
+    {
+        new StructData("KID", 50, 70.5f),
+        new StructData("TOM", 30, 20.2f)
+    };
+
+    public StructData[] charactersArray1 =
+    {
+        new StructData("KID", 50, 70.5f),
+        new StructData("TOM", 30, 20.2f)
+    };
+
+    public StructData[] charactersArray2;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            character2 = character1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            character2.lv = 1;
+
+            print("<color=blue>Character 2 Lv : " + character2.lv + "</color>");
+            print("<color=blue>Character 1 Lv : " + character1.lv + "</color>");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            characters[1] = characters[0];
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            characters[1].lv = 1;
+
+            print("<color=red>Character 2 Lv : " + characters[1].lv + "</color>");
+            print("<color=red>Character 1 Lv : " + characters[0].lv + "</color>");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            // 陣列會變為參考型別
+            charactersArray2 = charactersArray1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            charactersArray2[1].lv = 1;
+
+            print("<color=yellow>Character 2 Lv : " + charactersArray2[1].lv + "</color>");
+            print("<color=yellow>Character 1 Lv : " + charactersArray1[1].lv + "</color>");
+        }
+    }
+}
